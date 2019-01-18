6d143e0d816d353b42d0b92146ccd62922fc7dfe
zfog <zfog@qq.com>
2019-01-18 17:32:53 +0800

    add ddl.sql

---
+++ ddl.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `t_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户ID，手机号码',
+  `nickname` varchar(255) NOT NULL,
+  `password` varchar(32) DEFAULT NULL COMMENT 'MD5(MD5(pass明文+固定salt) + salt)',
+  `salt` varchar(10) DEFAULT NULL,
+  `head` varchar(128) DEFAULT NULL COMMENT '头像，云存储的ID',
+  `register_date` datetime DEFAULT NULL COMMENT '注册时间',
+  `last_login_date` datetime DEFAULT NULL COMMENT '上次登录时间',
+  `login_count` int(11) DEFAULT NULL COMMENT '登陆次数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
